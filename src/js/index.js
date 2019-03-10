b29f86350086a9316c2ead916caebe6ed463fe93
Yipee-ki-yay <mopoz07@gmail.com>
2019-03-10 22:36:07 +0200

    change text

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,38 @@ var test = 'include js error';
 
 $(document).ready(function() {
 	console.log('document ready');
-
 	// =================Include Modules==============================
 	@@include('./modules/testModule.js');
 	// =============================================================
-
 	if ($('body')) {console.log('jQuery OK')}
-	console.log(test); 
+
+	$.fn.randomize = function(selector){
+		var $elems = selector ? $(this).find(selector) : $(this).children(),
+				$parents = $elems.parent();
+	
+		$parents.each(function(){
+				$(this).children(selector).sort(function(){
+						return Math.round(Math.random()) - 0.5;
+				}).detach().appendTo(this);
+		});
+	
+		return this;
+	};
+	
+	// randomize the slides and then call slick
+	$('.slider').randomize('.slider__item');
+
+	$('.slider').slick({
+		autoplay: true,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		dots: false,
+		arrows: false,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+  });
 
 });
 
